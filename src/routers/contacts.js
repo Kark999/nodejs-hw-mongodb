84642b92771baffd48d58d0de19b05b90bfd5824
Andrey Svyrskyi <akkad1v@gmail.com>
2024-05-31 06:09:28 +0300

    'update'

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,5 +1,7 @@
 import { Router } from 'express';
 import {
+  createContactController,
+  deleteContactController,
   getContactByIdController,
   getContactsController,
 } from '../controllers/contacts.js';
@@ -9,5 +11,7 @@ const router = Router();
 
 router.get('/contacts', ctrlWrapper(getContactsController));
 router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
+router.post('/contacts', ctrlWrapper(createContactController));
+router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
 
 export default router;
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -13,3 +13,12 @@ export const getContactById = async (contactId) => {
     return contact;
   }
 };
+export const createContact = async (payload) => {
+  const contact = await Contact.create(payload);
+  return contact;
+};
+
+export const deleteContact = async (contactId) => {
+  const contact = await Contact.findOneAndDelete({ _id: contactId });
+  return contact;
+};
