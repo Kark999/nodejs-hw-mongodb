db29ff44b12d982f2e95cbe7bca8c48b6aff3739
Andrey Svyrskyi <akkad1v@gmail.com>
2024-06-07 02:38:18 +0300

    'update'

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -1,26 +1,9 @@
 import Joi from 'joi';
 
 export const createContactSchema = Joi.object({
-  name: Joi.string().min(3).max(20).required().messages({
-    'string.base': 'Username should be a string',
-    'string.min': 'Username should have at least {3} characters',
-    'string.max': 'Username should have at most {20} characters',
-    'any.required': 'Username is required',
-  }),
-
-  phoneNumber: Joi.string().min(3).max(20).required().messages({
-    'string.base': 'Phone number should be a string',
-    'string.min': 'Phone number should have at least {3} characters',
-    'string.max': 'Phone number should have at most {20} characters',
-    'any.required': 'Phone number is required',
-  }),
-
-  email: Joi.string().email().min(3).max(20).required().messages({
-    'string.base': 'Email should be a string',
-    'string.min': 'Email should have at least {3} characters',
-    'string.max': 'Email should have at most {20} characters',
-    'any.required': 'Email is required',
-  }),
+  name: Joi.string().min(3).max(20).required(),
+  phoneNumber: Joi.string().min(3).max(20).required(),
+  email: Joi.string().email().min(3).max(20).required(),
   isFavourite: Joi.boolean(),
   contactType: Joi.string().min(3).max(20).required(),
 });
