315044ed19c038332d9432c0789b387efda5dd9d
Andrey Svyrskyi <akkad1v@gmail.com>
2024-07-02 03:20:52 +0300

    'update'

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ import router from './routers/index.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import cookieParser from 'cookie-parser';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 const PORT = Number(env('PORT', '3000'));
 
@@ -27,6 +28,8 @@ export const setupServer = () => {
       message: 'Hello world!',
     });
   });
+
+  app.use(swaggerDocs());
 
   app.use(router);
 
